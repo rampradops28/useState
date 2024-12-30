cde2439e496f8e44559fb55cb7e410f0b37d609f
RAMPRADOP S <119684694+rampradops28@users.noreply.github.com>
2024-12-30 11:12:09 +0530

    Update State.jsx

--- components/State.jsx
+++ components/State.jsx
@@ -1,40 +1,6 @@
 import React, { useState } from 'react'
 
 const State = () => {
-//     const [name,setName] = useState('');
-//   return (
-//      <div>
-//         <button onClick={()=>{
-//             if(name == ''){
-//                 setName('ram')
-//             }
-//             else{
-//                 setName('')
-//             }
-               
-//         }}>CLICK ME</button>
-//         <div>{name}</div>
-//      </div>
-//   )
-    // const [count,setCount] = useState(0);
-    // return (
-    // <div>
-    //     <button onClick={()=>{
-    //         setCount(count+1)
-                
-    //     }}>Increament</button>
-    //     <button onClick={()=>{
-    //         setCount(count-1)
-                
-    //     }}>Decreament</button>
-    //     <button onClick={()=>{
-    //         setCount(0)
-                
-    //     }}>Reset</button>
-    //     <h1>Count: {count}</h1>
-    // </div>
-    // )
-
     const [name,setName] = useState('');
     return (
     <div>
